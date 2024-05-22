68b6b06b2648a929b4d0f9399bd8ddc3d9a0935c
Coolsugar <70053010+Coolsugar@users.noreply.github.com>
2024-05-23 01:04:47 +0530

    Add files via upload

---
+++ pendu_de_polar.py
@@ -0,0 +1,46 @@
+import numpy as np
+from scipy.integrate import odeint
+import time as tm
+print("Solving the non linear second order differential equation for a single pendulum...")
+
+def system(v, t, Omega):  # defining the eqation of motion where ω^2 = Ω
+  theta, theta_dot = v # Unpack state vector
+  d_theta_dot_dt = -Omega * np.sin(theta)  # Derivative of theta
+  d_theta_dt = theta_dot
+  return [d_theta_dt, d_theta_dot_dt]  # Return derivatives as a list
+
+# Initial states:
+theta0 = np.pi/2  #amplitude if theta_dot0 = 0
+theta_dot0 = 0  # Initial angular velocity
+v0 = [theta0, theta_dot0]
+
+Omega = 4 # value of ω^2
+
+# Solve the system
+t = np.linspace(0, 30, 3000)
+
+start_time = tm.time() # timing the operation
+
+sol = odeint(system, v0, t, args=(Omega,)) # solving the DE
+
+end_time = tm.time() # timing the operation
+
+theta = sol[:, 0]
+theta_dot = sol[:, 1]
+
+# Plot the solution
+import matplotlib.pyplot as plt
+
+# plot 3:
+r = []
+for _ in range(3000):
+    r.append(10)
+    
+ax = plt.subplot(111, projection='polar')
+ax.set_theta_offset(np.pi*1.5)
+ax.plot(theta , r , color = 'green')
+plt.title(f"Polar Plot  - Postions of CG of pendulum")
+plt.grid()
+plt.show()
+
+print(f"The operation took {end_time - start_time} seconds.")
