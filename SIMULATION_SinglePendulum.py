ca59e4f0211b31d42a5b682c2ef794034ea71159
Coolsugar <70053010+Coolsugar@users.noreply.github.com>
2024-05-23 14:22:41 +0530

    Update SIMULATION_SinglePendulum.py

--- SIMULATION_SinglePendulum.py
+++ SIMULATION_SinglePendulum.py
@@ -14,13 +14,18 @@ def system(v, t, Omega):  # defining the eqation of motion where ω^2 = Ω
   return [d_theta_dt, d_theta_dot_dt]  # Return derivatives as a list
 
 # Initial states:
-theta0 = np.pi/2  #amplitude if theta_dot0 = 0
+theta0 = np.pi/6  #amplitude if theta_dot0 = 0
 theta_dot0 = 0  # Initial angular velocity
 v0 = [theta0, theta_dot0]
 
-Omega = 2 # value of ω^2
-FPS = 40 # set frames per second
+g = 9.81
+l = 6.9377
+
+Omega = g/l # value of ω^2
+FPS = 60 # set frames per second
 sim_time = 30 # total simulated 
+
+mass = 2
 
 t = np.linspace(0, sim_time , sim_time*FPS )
 
@@ -36,18 +41,26 @@ theta_dot = sol[:, 1]
 ex = []  # x coordinates
 why = [] # y coordinates
 for i in theta:
-    ex.append(np.cos(i - np.pi/2))
-    why.append(np.sin(i - np.pi/2))
-xe = [-0.4 , 0.4]
+    ex.append(l*np.cos(i - np.pi/2))
+    why.append(l*np.sin(i - np.pi/2))
+xe = [-5 , 5]
 yhw = [0 , 0]
-radius = 0.2
+
+radius = 1
 
 # graphing results:
-def graph(n): # plotting a graph for every frame n 
-    a = np.cos(n - np.pi/2) # x coordinate 
-    b = np.sin(n - np.pi/2) # y coordinate 
+def graph(n , m): # plotting a graph for every frame n 
+    a = l*np.cos(n - np.pi/2) # x coordinate 
+    b = l*np.sin(n - np.pi/2) # y coordinate 
     
-    center = (a , b)
+    PE = mass*g*(l+b) # potential energy of system
+    KE = 0.5*mass*(theta_dot[m]*l)**2  # kinetic enegry of system 
+    TE = KE + PE # total energy of system 
+    
+    
+    
+    center = (a , b) # CG of pendulum
+    theta_deg = theta[m]*180/np.pi # value of theta in degrees
     
     circle = Circle(xy=center, radius=radius)
     fig, ax = plt.subplots() # the pendulum with  its CG at its center
@@ -62,24 +75,19 @@ def graph(n): # plotting a graph for every frame n
     plt.plot(ex , why , color = 'grey') # plotting path to be traced
     plt.plot(xe , yhw , color = 'purple') #so that the string isn't hanging from nothing
     
-    plt.xlim(-1.5 , 1.5) # static limits
-    plt.ylim(-1.5 , 0.5) 
-    plt.title(f"Simulation of a pendulum  d²θ/dt² + {Omega}sin(θ) = 0 with initial conditions θ(0) = {theta0:.4f} and θ'(0) = {theta_dot0}")
+    plt.xlim(-8 , 8) # static limits
+    plt.ylim(-8 , 0.5)
+    plt.xlabel(f"θ = {theta_deg:.3f}° \n System Energy: \n Kinetic Enegery = {KE:.4f} \n Potential Enegery = {PE:.4f} \n Total Enegery = {TE:.4f}")
+    plt.title(f"Simulation of a pendulum  d²θ/dt² + {Omega:.3f}sin(θ) = 0 with initial conditions θ(0) = {theta0:.3f} and θ'(0) = {theta_dot0} \n X - Y Space: ")
     plt.grid()
     plt.show()
  
 st_2 = tm.time()
-for n in theta: # make graph for every value of theta 
-    graph(n)
+for n in range(len(theta)): # make graph for every value of theta 
+    graph(theta[n] , n)
 et_2 = tm.time()
 
 delta_t2 = et_2 - st_2 
 
 print(f"> All {sim_time*FPS} graphs (frames) have been generated.")
 print(f"> Real time taken to generate {sim_time} seconds of simulated time is {delta_t2:.4f} seconds. Ratio = {delta_t2 / sim_time}, the real time taken to generate 1 second of simulated time.")
-    
-
-    
-    
-    
-
